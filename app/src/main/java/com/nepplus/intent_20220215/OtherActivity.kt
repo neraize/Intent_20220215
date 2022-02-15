f3506047d8a67e6f93f5737815b3721d0b8280b4
seojungmin <neraize@gmail.com>
2022-02-15 16:59:34 +0900

    finish()로 이전 화면으로 복귀

--- app/src/main/java/com/nepplus/intent_20220215/OtherActivity.kt
+++ app/src/main/java/com/nepplus/intent_20220215/OtherActivity.kt
@@ -1,11 +1,21 @@
 package com.nepplus.intent_20220215
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_other.*
 
 class OtherActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_other)
+
+        btnReturnToMain.setOnClickListener {
+            // 돌아가는 경우에는 Intent 활용x
+//            val myIntent = Intent(this, MainActivity::class.java)
+//            startActivity(myIntent)
+
+            finish()
+        }
     }
 }
